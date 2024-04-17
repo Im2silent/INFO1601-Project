162918647033337183b1d246b67b83b02beabfc2
Im2silent <34144500+Im2silent@users.noreply.github.com>
2024-04-16 23:31:35 -0400

    Update fireBaseConfig.js

--- public/fireBaseConfig.js
+++ public/fireBaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB6Ta5Zvph7dkXo9rzYLkuqX_bII2k08MM",
-    authDomain: "we-go-cook.firebaseapp.com",
-    projectId: "we-go-cook",
-    storageBucket: "we-go-cook.appspot.com",
-    messagingSenderId: "250112919629",
-    appId: "1:250112919629:web:e0ca7d235d3b577f0d3404"
-  };
+  apiKey: "AIzaSyAGnGQ5tAX6KEHL1RyqAf6VII7X4MacZmg",
+  authDomain: "are-we-cooking.firebaseapp.com",
+  projectId: "are-we-cooking",
+  storageBucket: "are-we-cooking.appspot.com",
+  messagingSenderId: "776897844103",
+  appId: "1:776897844103:web:76cb639bf2a3683b953922"
+};
 
   export default firebaseConfig;
