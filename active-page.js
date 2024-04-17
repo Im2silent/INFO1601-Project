5fc38b3ea0c1dba382c866070dac9a6b672fa7a4
Im2silent <34144500+Im2silent@users.noreply.github.com>
2024-04-16 20:19:36 -0400

    Delete active-page.js

--- active-page.js
+++
@@ -1,8 +0,0 @@
-const acivePage = window.location.pathname;
-const navLinks = document.querySelectorAll("nav a").
-forEach(link =>{
-  if(link.href.includes(`${acivePage}`)){
-    link.classList.add("active");
-    
-  }
-})
